4b40b4be4ac9fbc2b66a581e5790bcd40d2404b4
jihostudy <cale.jiho@gmail.com>
2025-03-14 14:00:47 +0900

    feat: 컴포넌트 구성 변경 (인라인 컴포넌트 구현 방법)

--- components/common/PortalComponent.js
+++ components/common/PortalComponent.js
@@ -1,13 +1,11 @@
-class InlineComponent {
+class PortalComponent {
   $target
   $props
   $state
   $elements // 사용하는 DOM 요소들 (재사용성을 위해 설정)
 
-  constructor($target, $props) {
-    this.$target = $target
+  constructor($props) {
     this.$props = $props
-
     this.setup()
     this.render()
     this.setEvent()
@@ -23,39 +21,15 @@ class InlineComponent {
   /** 컴포넌트에서 필요한 이벤트 설정 */
   setEvent() {}
 
-  /** 이벤트 등록 추상화 */
-  /** 이벤트 등록 추상화 (이제 selector 대신 요소를 직접 받음) */
-  addEvent(eventType, element, callback) {
-    if (!element) {
-      console.error(`❌ 이벤트를 추가할 요소가 존재하지 않습니다.`)
-      return
-    }
-
-    element.addEventListener(eventType, event => {
-      callback(event)
-    })
-  }
-
   /** 템플릿을 실제 요소로 변환  */
   render() {
-    const wrapper = document.createElement('div') // 임시 래퍼 요소
+    const wrapper = document.createElement('div') // 임시 요소
     wrapper.innerHTML = this.template().trim()
+    this.$target = wrapper.firstChild
 
-    const newComponent = wrapper.firstElementChild // 첫 번째 자식 요소를 가져옴
-    if (!newComponent) return
+    if (!this.$target) return
 
-    this.$target.replaceWith(newComponent)
-    this.$target = newComponent
-    // id 속성 추가
-    if (this.$props?.idName) {
-      this.$target.id = this.$props.idName
-    }
-    // class 속성 추가
-    if (this.$props?.className) {
-      this.$target.classList.add(...this.$props.className.split(' '))
-    }
-
-    this.mounted() // mounted() 실행
+    this.mounted()
   }
 
   /** 자식 컴포넌트 및 요소 정의 */
@@ -75,7 +49,6 @@ class InlineComponent {
     const link = document.createElement('link')
     link.rel = 'stylesheet'
     link.href = stylePath
-    link.setAttribute('data-dynamic-style', stylePath) // 동적 스타일 태그 관리
     document.head.appendChild(link)
   }
 
@@ -88,4 +61,4 @@ class InlineComponent {
   }
 }
 
-export default InlineComponent
+export default PortalComponent
